b621a80df0c395bd8a53b4ddc4aae194f73a82d6
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-13 13:33:55 +0100

    init configuration

--- app/src/main/java/org/m2i/readerapp/MainActivity.kt
+++ app/src/main/java/org/m2i/readerapp/MainActivity.kt
@@ -12,26 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 import org.m2i.readerapp.ui.theme.ReaderAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderAppTheme {
-
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap();
-                user["firstname"] = "Joao"
-                user["lastname"] = "marco"
-                db.collection("users")
-                    .add(user)
-                    .addOnSuccessListener {
-                        Log.d("UserAdd","OnCreate $it")
-                    }.addOnFailureListener{
-                        Log.d("UserAdd","OnCreate $it")
-                    }
-
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
---
+++ app/src/main/java/org/m2i/readerapp/ReaderApplication.kt
@@ -0,0 +1,8 @@
+package org.m2i.readerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication : Application() {
+}
